599508ea7870adfe60e8ef362cdcd900c5e9bda3
ahyani12 <79136159+ahyani12@users.noreply.github.com>
2021-02-20 15:34:28 +0700

    spk siswa teladan ahyani
    SPK Penentuan Siswa Teladan SD N 1 Kokosan

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -2,8 +2,8 @@
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
       <!-- Brand Logo -->
       <a href="index3.html" class="brand-link">
-        <img src="<?= base_url('assets'); ?>/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">AdminLTE 3</span>
+        <img src="<?= base_url('assets'); ?>/gambar/logo_sd.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <span class="brand-text font-weight-light">SD N 1 KOKOSAN </span>
       </a>
 
       <!-- Sidebar -->
@@ -11,7 +11,7 @@
         <!-- Sidebar user panel (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
           <div class="image">
-            <img src="<?= base_url('assets'); ?>/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+            <img src="<?= base_url('assets'); ?>/gambar/logo_sd.png" class="img-circle elevation-2" alt="User Image">
           </div>
           <div class="info">
             <center><a href="#" class="d-block ml-3"><?= $user['nama_guru']; ?></a></center>
